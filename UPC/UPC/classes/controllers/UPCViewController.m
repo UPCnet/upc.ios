b578fb0efc9c9664c4fb71549353423565901416
jose.gonzalez <jose.gonzalez@rudelabs.com>
2012-07-17 13:14:13 +0200

    New option in main menu (Másters i postgraus de formació contínua)

--- UPC/UPC/classes/controllers/UPCViewController.m
+++ UPC/UPC/classes/controllers/UPCViewController.m
@@ -33,8 +33,8 @@
 
 - (void)viewDidLoad
 {
-    self.responsiveURLs = [NSDictionary dictionaryWithObjectsAndKeys:@"http://www.upc.edu/grau/graus.php?lang=ca", @"graus", @"http://www.upc.edu/master/masters.php?lang=ca", @"masters", @"http://m.atenea.upc.edu?app=true&lang=ca", @"atenea", nil];
-    self.titles = [NSDictionary dictionaryWithObjectsAndKeys:@"Graus", @"graus", @"Màsters universitaris", @"masters", @"Atenea", @"atenea", nil];
+    self.responsiveURLs = [NSDictionary dictionaryWithObjectsAndKeys:@"http://www.upc.edu/grau/graus.php?lang=ca", @"graus", @"http://www.upc.edu/master/masters.php?lang=ca", @"masters", @"http://m.formaciocontinua.upc.edu/cat", @"mastersProfessionals", @"http://m.atenea.upc.edu?app=true&lang=ca", @"atenea", nil];
+    self.titles = [NSDictionary dictionaryWithObjectsAndKeys:@"Graus", @"graus", @"Màsters universitaris", @"masters", @"Formació permanent", @"mastersProfessionals", @"Atenea", @"atenea", nil];
     [super viewDidLoad];
 }
 
@@ -54,7 +54,7 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    if ([segue.identifier isEqualToString:@"graus"] || [segue.identifier isEqualToString:@"masters"] || [segue.identifier isEqualToString:@"atenea"]) {
+    if ([segue.identifier isEqualToString:@"graus"] || [segue.identifier isEqualToString:@"masters"] || [segue.identifier isEqualToString:@"mastersProfessionals"] || [segue.identifier isEqualToString:@"atenea"]) {
         UPCResponsiveWebViewController *webViewController = (UPCResponsiveWebViewController *)[segue destinationViewController];
         webViewController.url = [NSURL URLWithString:[self.responsiveURLs objectForKey:segue.identifier]];
         webViewController.navigationItem.title = [self.titles objectForKey:segue.identifier];
